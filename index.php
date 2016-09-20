45346e80cca8a942bbd0164709a684d3b180d66b
JDavidsson <johan.et.davidsson@gmail.com>
2016-09-20 12:10:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
 
 include "header.html";
+include "db_interface.php"; 
+include "mysql_login_data.php"; // unsafe to have login data here?
+
+$database new Database( $host,$userName, $password, $database);
+$database->openConnection();
+
+
+if(! $database->isConnected()) {
+  header ("Location: db_not_connected.php");
+  exit();
+}
+
+//connection successfull
+
+$result = $database->getAllProducts();
 
 ?>
 
@@ -13,13 +28,30 @@ include "header.html";
 <body>
 
 <h1>Shop Name</h1>
-<div>
-  <img src="#" alt="Item name">
-  <h3>Item name</h3>
-  <p>Item description</p>
-  <p>Price: ..</p>
-  <a href="#"><button>Add to cart</button></a>
-</div>
+<div class="product_container"></div>
+ 
+<?php    
+foreach ( $result as $row ) {
+     
+    /*
+    echo $row[0]; // productId
+    echo $row[1]; // name
+    echo $row[2]; // desc
+    echo $row[3]; // price
+    echo $row[4]; //color
+    */
+     
+echo "
+    <div class=\"product-div\">
+    <div class=\"color-image\" style=\"background-color:$row[4]\"></div>
+    <p class=\"product-name\">$row[1]<p>
+    <p class=\"product-desc\">$row[2]</p>
+    <p class=\"product-price\">Price: $row[3]</p>
+    <a href=\"#\"><button>Add to cart</button></a>
+    </div>";
+     
+}
+?>
 
 <p>We use cookies!</p>
 
