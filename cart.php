55a334c5340adba0d07c02cee228fc77cd15e380
Johan Davidsson <johan.et.davidsson@gmail.com>
2016-09-27 10:21:48 +0200

    add cookie check

--- cart.php
+++ cart.php
@@ -3,7 +3,28 @@
 include "header.php";
 include "db_interface.php";
 include "mysql_login_data.php";
-$items = json_decode($_COOKIE["items"], true);
+
+if (isset($_COOKIE["items"])) {
+
+	$items = json_decode($_COOKIE["items"], true);
+	$database = new Database( $host,$userName, $password, $database);
+	$database->openConnection();
+	$toPay = 0;
+	echo "In cart: \n";
+
+	foreach ($items as $colorID => $nbr) {
+	  $col = $database->getItem($colorID);
+	  $item = $col[0];
+	  echo "<p>$nbr of the color $item[1] for a total of ". ($nbr*$item[3]) ."</p>";
+	  $toPay+=$nbr*$item[3];
+	}
+
+	echo "\nTotal: $toPay \n";
+	$database->closeConnection();
+
+
+}
+
 
 /*
 echo $row[0]; // productId
@@ -12,18 +33,6 @@ echo $row[2]; // desc
 echo $row[3]; // price
 echo $row[4]; //color
 */
-$database = new Database( $host,$userName, $password, $database);
-$database->openConnection();
-$toPay = 0;
-echo "In cart: \n";
-foreach ($items as $colorID => $nbr) {
-  $col = $database->getItem($colorID);
-  $item = $col[0];
-  echo "<p>$nbr of the color $item[1] for a total of ". ($nbr*$item[3]) ."</p>";
-  $toPay+=$nbr*$item[3];
-}
-echo "\nTotal: $toPay \n";
-$database->closeConnection();
 
 if(isset($_SESSION["loggedIn"]))
   echo "<a href=\".\checkout.php\"><button>Go to payment</button></a>";
