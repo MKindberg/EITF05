e24b6717b06f1e6b5deb4ff49a5bd11944983d18
JDavidsson <johan.et.davidsson@gmail.com>
2016-09-30 13:22:40 +0200

    Delete signOut.php

--- signOut.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include "db_interface.php";
-    include "mysql_login_data.php";
-    $database = new Database( $host,$userName, $password, $database);
-    $database->signOut();
-    header('Location: ' . 'index.php');
-    die();
-?>
