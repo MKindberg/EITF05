3e5e4cf99fbb2f06772c8eed4f855a5162734f5b
Johan Davidsson <johan.et.davidsson@gmail.com>
2016-09-30 09:53:39 +0200

    certs

--- certs/generate_crt.sh
+++ certs/generate_crt.sh
@@ -1,3 +1,4 @@
 
-openssl req -x509 -newkey rsa:4096 -keyout ssl.key -out server.crt -days 100
+openssl req -x509 -newkey rsa:4096 -keyout ssl.key -out server.crt -days 100 -nodes
 
+# -nodes because xamp couldn´t configure with password to private key
